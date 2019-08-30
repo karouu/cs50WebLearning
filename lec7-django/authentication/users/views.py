1d9a6a02ff53f0ca2186d7db19b2406b2b37c719
matter <chenda721@gmail.com>
2019-08-30 16:54:49 +0800

    fix 0830

--- lec7-django/authentication/users/views.py
+++ lec7-django/authentication/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
+from django.shortcuts import render
 
 # Create your views here.
 
